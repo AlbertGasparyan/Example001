3e03a73857d3ee7c3a955a58c0637ae82641c7c2
Albert <mona-m1@ya.ru>
2022-10-15 11:46:05 +0600

    add masha

---
+++ Example001_HelloConsole/Example005_masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Create name user");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="masha")
+{
+    Console.WriteLine("Yeeee,It's MAsha");
+}
+else
+{
+    Console.Write("Hello,");
+    Console.Write(username);
+}
